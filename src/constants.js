7d24a4ba8e52a050137058b10bb333d3c9398333
devjob4475 <thananchai216@gmail.com>
2024-04-04 15:07:59 +0700

    'test'

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/Java these days.
+    I've made a couple of games in that language. I also like Node.js and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/devjob4475" target="_blank">Github</a>!`,
+  "cs-degree": `Hello, It's Thananchai Sittikun, an Thai Web Developer, Graduated with Bachelors of Software Engineer (2019)`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  I also make programming tutorials on Medium. Go sub to <a href="https://medium.com/@thananchai.sskru" target="_blank">my medium</a>! (If you like the content)
+  You'll learn how I built this portfolio you're currently playing through!`,
+  tv: `I enjoy watching TV, especially comedy and fantasy movies.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/devjob4475/resume/blob/main/thananchai.jpg?raw=true" target="_blank">Check it out?</a>
+  Contact me at thananchai@gmail.com if you have any interesting job opportunities!`,
+  projects: `I've worked as a Front-End Web Developer for 1+ years. I am currently studying Full Stack Web Development Course, finished the font-end part (React.js, ,Next.js), and started the back-end (Node.js). <a href="https://thananchai.vercel.app" target="_blank">here</a>.`,
+  library: `This is my bookshelf that collects many books I really like, and programming books that I really enjoy reading.`,
+  exit: `If you want to exit Devjob1234's portfolio, just close the tab.`,
+};
